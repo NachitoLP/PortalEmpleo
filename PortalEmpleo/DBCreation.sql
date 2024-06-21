946d3833fe546565a7647a973014d662d83efa98
Joaquín Lopez <joaquinacho@live.com>
2024-06-21 01:03:01 -0300

    Archivo de creación de base SQL

--- PortalEmpleo/DBCreation.sql
+++ PortalEmpleo/DBCreation.sql
@@ -3,56 +3,76 @@ GO
 
 USE PortalEmpleo
 
-CREATE TABLE Role (
-	role_description varchar(30) PRIMARY KEY
-)
+CREATE TABLE [dbo].[Category] (
+    [category_id]   INT            IDENTITY (1, 1) NOT NULL,
+    [category_name] NVARCHAR (100) NOT NULL,
+    PRIMARY KEY CLUSTERED ([category_id] ASC),
+    UNIQUE NONCLUSTERED ([category_name] ASC)
+);
 
-CREATE TABLE Skills (
-	skill_name varchar(40) PRIMARY KEY
-)
+CREATE TABLE [dbo].[Role] (
+    [role_description] VARCHAR (30) NOT NULL,
+    PRIMARY KEY CLUSTERED ([role_description] ASC)
+);
 
-CREATE TABLE Users (
-	user_id int PRIMARY KEY IDENTITY,
-	user_name varchar(30) NOT NULL,
-	user_surname varchar(30) NOT NULL,
-	user_email nvarchar(150) NOT NULL,
-	user_password nvarchar(16) NOT NULL,
-	user_birth_date date NOT NULL,
-	user_profile_img VARBINARY(MAX),
-	user_age int,
-	role_description varchar(30) NOT NULL,
-	FOREIGN KEY (role_description) REFERENCES Role(role_description)
-)
+CREATE TABLE [dbo].[Skills] (
+    [skill_name] VARCHAR (40) NOT NULL,
+    PRIMARY KEY CLUSTERED ([skill_name] ASC)
+);
 
-CREATE TABLE Companies (
-	company_id int PRIMARY KEY IDENTITY,
-	company_name varchar(100) NOT NULL,
-	company_email nvarchar(150) NOT NULL,
-	description text
-)
+CREATE TABLE [dbo].[Users] (
+    [user_id]                INT             IDENTITY (1, 1) NOT NULL,
+    [user_name]              VARCHAR (30)    NOT NULL,
+    [user_surname]           VARCHAR (30)    NOT NULL,
+    [user_email]             NVARCHAR (150)  NOT NULL,
+    [user_password]          VARCHAR (255)   NULL,
+    [user_birth_date]        DATE            NOT NULL,
+    [user_profile_img]       VARBINARY (MAX) NULL,
+    [user_age]               INT             NULL,
+    [role_description]       VARCHAR (30)    NOT NULL,
+    [user_password_salt]     VARCHAR (255)   NOT NULL,
+    [user_title_description] VARCHAR (200)   NULL,
+    PRIMARY KEY CLUSTERED ([user_id] ASC),
+    FOREIGN KEY ([role_description]) REFERENCES [dbo].[Role] ([role_description])
+);
 
-CREATE TABLE Posts (
-	post_id INT PRIMARY KEY IDENTITY,
-	post_title varchar(40) NOT NULL,
-	post_description text NOT NULL,
-	post_image varchar(255),
-	user_id int NOT NULL,
-	FOREIGN KEY (user_id) REFERENCES Users(user_id)
-)
+CREATE TABLE [dbo].[Companies] (
+    [company_id]    INT            IDENTITY (1, 1) NOT NULL,
+    [company_name]  VARCHAR (100)  NOT NULL,
+    [company_email] NVARCHAR (150) NOT NULL,
+    [description]   TEXT           NULL,
+    PRIMARY KEY CLUSTERED ([company_id] ASC)
+);
 
-CREATE TABLE users_skills (
-	user_id int,
-	skill_name varchar(40),
-	FOREIGN KEY (user_id) REFERENCES Users(user_id),
-	FOREIGN KEY (skill_name) REFERENCES Skills(skill_name)
-)
+CREATE TABLE [dbo].[Posts] (
+    [post_id]          INT             IDENTITY (1, 1) NOT NULL,
+    [post_title]       VARCHAR (40)    NOT NULL,
+    [post_description] TEXT            NOT NULL,
+    [user_id]          INT             NOT NULL,
+    [category_id]      INT             NULL,
+    [post_image]       VARBINARY (MAX) NULL,
+    [post_date]        DATE            NULL,
+    PRIMARY KEY CLUSTERED ([post_id] ASC),
+    FOREIGN KEY ([user_id]) REFERENCES [dbo].[Users] ([user_id]),
+    FOREIGN KEY ([category_id]) REFERENCES [dbo].[Category] ([category_id])
+);
 
-CREATE TABLE users_companies (
-	user_id int,
-	company_id int,
-	FOREIGN KEY (user_id) REFERENCES Users(user_id),
-	FOREIGN KEY (company_id) REFERENCES Companies(company_id)
-)
+CREATE TABLE [dbo].[users_skills] (
+    [user_id]    INT          NULL,
+    [skill_name] VARCHAR (40) NULL,
+    FOREIGN KEY ([user_id]) REFERENCES [dbo].[Users] ([user_id]),
+    FOREIGN KEY ([skill_name]) REFERENCES [dbo].[Skills] ([skill_name])
+);
+
+CREATE TABLE [dbo].[users_companies] (
+    [user_id]    INT NULL,
+    [company_id] INT NULL,
+    FOREIGN KEY ([user_id]) REFERENCES [dbo].[Users] ([user_id]),
+    FOREIGN KEY ([company_id]) REFERENCES [dbo].[Companies] ([company_id])
+);
+
+
+/*Crear procedimientos en una secuencia a parte*/
 
 CREATE PROCEDURE [dbo].[LoginUser]
 	@user_email nvarchar(150),
